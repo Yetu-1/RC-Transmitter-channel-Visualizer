92e2503fe178548ec2878a809c7013faa7abe333
Yetu <davidsalihu@gmail.com>
2021-07-10 02:10:38 +0100

    added interrupt subroutine

--- timerConfig.h
+++ timerConfig.h
@@ -4,6 +4,7 @@
  *  Created on: Jul 7, 2021
  *      Author: Yetu
  */
+
 #include <stm32h745xx.h>
 
 #ifndef INC_TIMERCONFIG_H_
@@ -12,6 +13,12 @@
 
 
 #endif /* INC_TIMERCONFIG_H_ */
+
+// variables for receiver input
+int32_t channel_1_start, channel_1;
+int32_t channel_2_start, channel_2;
+int32_t channel_3_start, channel_3;
+int32_t channel_4_start, channel_4;
 
 void timerGpio_Config(void);
 
